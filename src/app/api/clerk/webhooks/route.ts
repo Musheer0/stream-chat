62267896f70b1ecd9538a64f2a047bbb912f216d
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-09-07 12:03:06 +0530

    intial

---
+++ src/app/api/clerk/webhooks/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import wh from '@clerk/nextjs/webhooks'
+import { StreamChat } from "stream-chat"
+
+const serverClient = StreamChat.getInstance(
+  process.env.STREAM_API_KEY!,
+  process.env.STREAM_API_SECRET!
+)
+export const POST = async (req:NextRequest)=>{
+    try {
+        const {type,data} =await wh.verifyWebhook(req,{signingSecret:process.env.CLERK_WH})
+    
+    if(!(type==='user.created'||type==='user.updated'))
+    return NextResponse.json({success:false,error:'un-authorized webhook'},{status:400})
+    const user_agent = req.headers.get('user-agent')
+    const {id,first_name,last_name,image_url,phone_numbers,email_addresses,primary_phone_number_id,primary_email_address_id} = data
+    const new_user = {
+        id,
+        name:first_name+" "+last_name,
+        image:image_url,
+        email_address: email_addresses.find((e)=>e.id===primary_email_address_id)?.email_address||null,
+        phone_number:phone_numbers.find((e)=>e.id===primary_phone_number_id)?.phone_number||null,
+        user_agent,
+        ip:req.headers.get('ip')
+    }
+    try {
+        if(serverClient && (new_user.email_address||new_user.phone_number)){
+        await serverClient.upsertUser(new_user)
+    }
+    } catch (error) {
+    console.error('[stream sdk error]',error)   
+    return NextResponse.json({success:false,error})
+    }
+    return NextResponse.json(
+    { success: true },
+    { status: type === "user.created" ? 201 : 200 }
+    )
+
+    } catch (error) {
+          console.error('[clerk webhook sdk error]',error)   
+    return NextResponse.json({success:false,error})
+    }
+}
---
+++ src/app/api/stream/user/get-token/route.ts
@@ -0,0 +1,33 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import { StreamChat } from "stream-chat"
+import { Redis } from '@upstash/redis'
+const redis = Redis.fromEnv()
+
+const serverClient = StreamChat.getInstance(
+  process.env.STREAM_API_KEY!,
+  process.env.STREAM_API_SECRET!
+)
+export const GET = async(req:NextRequest)=>{
+    try {
+        const session = await auth()
+        if(!session.userId)
+            return NextResponse.json({success:false,error:'Unauthorized'},{status:401})
+        const rate_limit_key = `rate-limit:${session.userId}`
+        const count = await redis.incr(rate_limit_key);
+         if (count === 1) {
+        await redis.expire(rate_limit_key, 60);
+        }
+        if (count > 5) {
+        return NextResponse.json(
+        { error: "Rate limit exceeded" },
+        { status: 429 }
+            );
+            }
+        const token = serverClient.createToken(session.userId)
+        return NextResponse.json({success:true,token,exp:55 * 60 * 1000})
+    } catch (error) {
+        console.log('[stream sdk error]',error)
+        return NextResponse.json({success:false,error:'Internal server error'},{status:500})
+    }
+}
